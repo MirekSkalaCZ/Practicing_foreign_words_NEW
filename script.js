fbf3469c188936fdb3653bb288a5c4ee061f6ff3
Mirek Skála <mirek.skala@seznam.cz>
2022-11-03 07:25:57 +0100

    Created HTML, CSS and JS. Basic styles and functions

---
+++ script.js
@@ -0,0 +1,53 @@
+/**** Dark mode ****/
+
+let icon_dm = document.getElementById("icon_dm");
+
+if(localStorage.getItem("theme") == null){
+    localStorage.setItem("theme", "light"); 
+}
+
+let localData = localStorage.getItem("theme");
+
+if(localData == "light") {
+    icon_dm.src = "images/moon.png";
+    document.body.classList.remove("dark_theme");
+}   else if (localData == "dark") {
+    icon_dm.src = "images/sun.png";
+    document.body.classList.add("dark_theme");
+}
+
+icon_dm.addEventListener ("click", function(){
+    document.body.classList.toggle("dark_theme");
+    if(document.body.classList.contains("dark_theme")){
+        icon_dm.src = "images/sun.png";
+        localStorage.setItem("theme", "dark");
+    }   else {
+        icon_dm.src = "images/moon.png";
+        localStorage.setItem("theme", "light");
+    }
+});
+
+/**** Welcome Text ****/
+
+const heading = document.getElementById("text");
+
+const text = "Procvičování cizích slov";
+
+let idLetter = 1;
+
+let delay = 150;
+
+function printText(){
+    heading.innerText = text.slice(0,idLetter)
+    
+    idLetter++;
+    
+    if(idLetter > text.length){
+        idLetter = 1;
+    }
+    
+    setTimeout(printText, delay);
+
+}
+    
+printText();
